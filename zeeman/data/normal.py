a2ba436934016347dcac2ebbb36b24bfb6b92acf
Marian <mail@marian42.de>
2016-06-23 02:23:11 +0200

    Auswertung normaler Zeeman-Effekt

---
+++ zeeman/data/normal.py
@@ -0,0 +1,30 @@
+import numpy as np
+from uncertainties import ufloat
+from uncertainties import unumpy
+
+dispersionsgebiet = 48.9*10**(-12)
+h_bar = 6.62607 * 10**(-34)
+c = 299792458
+l = 643.8 * 10**(-9)
+mu_B = 9.274 * 10**(-24)
+B = ufloat(0.608, 0.014)
+
+Ds, ds = np.genfromtxt('normal.txt', delimiter='\t', skip_header=1, skip_footer=0, unpack=True)
+
+dlambda = 0.5 * ds / Ds * dispersionsgebiet
+
+deltaE = h_bar * c / l**2 * dlambda
+
+g_J = deltaE / (mu_B * B)
+
+for i in range(len(Ds)):
+	print(
+		"%.0f" % (Ds[i]) + " & " +
+		"%.0f" % (ds[i]) + " & " +
+		"%.2f" % (dlambda[i] * 10**12) + " & " +
+		"%.2f" % (g_J[i].nominal_value) + "\\pm" + "%.2f" % (g_J[i].std_dev) + "\\\\"
+	)
+
+mittelwert = np.mean(g_J)
+print(" ")
+print(mittelwert)
